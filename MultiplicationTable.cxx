8c9a5d8e08a97f2bcc780eede1a893687ba3a59b
Ibrahim Mohammed <36298932+Ib-Programmer@users.noreply.github.com>
2022-10-02 11:24:42 +0100

    Add files via upload

---
+++ MultiplicationTable.cxx
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int i, j, n;
+	
+	cout <<"Enter a number : "<<endl;
+	cin>>n;
+	 
+	for(i=1;i<=n;i++){
+	
+	for(j=1;j<=12;j++){
+
+
+	cout<<i<<" * "<<j<<" = "<<i*j<<endl;
+}
+cout<<endl<<endl;
+	}
+
+
+}
